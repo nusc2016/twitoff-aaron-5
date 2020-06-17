bc72cca313e2fcd91bcca5520ae616f4ffc87040
nusc2016 <61950417+nusc2016@users.noreply.github.com>
2020-06-17 13:05:16 -0700

    update to basilica

--- web_app/services/basilica_service.py
+++ web_app/services/basilica_service.py
@@ -1,17 +1,28 @@
 from basilica import Connection
+import os
+from dotenv import load_dotenv
 
-API_KEY = "ba063e4f-17ce-e0d0-43fc-943b39462921"
+load_dotenv
 
-sentences = [
-    "This is a sentence!",
-    "This is a similar sentence",
-    "I don't think this sentence is similar at all...",
-]
+API_KEY = os.getenv("BASILICA_API_KEY")
 
+# could use a function here to return our connection
+# could use a class
 connection = Connection(API_KEY)
 print("CONNECTION", type(connection))
 
-embeddings = list(connection.embed_sentences(sentences))
-print(embeddings)
+if __name__ == "__main__":
 
-breakpoint()
+    sentences = [
+        "This is a sentence!",
+        "This is a similar sentence",
+        "I don't think this sentence is similar at all...",
+    ]
+
+    embeddings = list(connection.embed_sentences(sentences))
+    print(embeddings)
+
+    embedding = connection.embed_sentence("Hello World!!!")
+    print(type(embedding))  #> <class 'list'>
+    print(type(embedding[0]))  #> <class 'float'>
+    print(len(embedding))  #> 768
