a93ac505c32dbdcf7db3e7421e27a171903aa438
nusc2016 <61950417+nusc2016@users.noreply.github.com>
2020-06-18 07:24:54 -0700

    made some changes

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -3,21 +3,21 @@ from flask import Flask
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
-# from web_app.twitter_routes import twitter_routes
+from web_app.routes.twitter_routes import twitter_routes
 
-DATABASE_URI = "sqlite:///twitoff_aaron_5.db"  # using relative filepath
+DATABASE_URL = "sqlite:///twitoff_aaron_5.db"  # using relative filepath
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     db.init_app(app)
     migrate.init_app(app, db)
 
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
-    # app.register_blueprint(twitter_routes)
+    app.register_blueprint(twitter_routes)
 
     return app
 
