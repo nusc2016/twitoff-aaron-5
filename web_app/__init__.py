56636804aba426ee38b0dbeb5d0f9d1494e10692
nusc2016 <61950417+nusc2016@users.noreply.github.com>
2020-06-18 18:45:56 -0700

    debug issues

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,15 +1,14 @@
-from dotenv import load_dotenv
-from flask import Flask
+# web_app/__init__.py
 
+import os
+
+from flask import Flask
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
-from web_app.routes.book_routes import book_routes
 from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.stats_routes import stats_routes
 
-load_dotenv()
-
-DATABASE_URL = "sqlite:///twitoff_aaron_5.db"  # using relative filepath
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 def create_app():
@@ -21,7 +20,6 @@ def create_app():
     migrate.init_app(app, db)
 
     app.register_blueprint(home_routes)
-    app.register_blueprint(book_routes)
     app.register_blueprint(twitter_routes)
     app.register_blueprint(stats_routes)
 
